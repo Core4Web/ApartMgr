a7ad91c58e6e451c5db0ccf1f41f2682d00923e3
Core4Web <core.4web@gmail.com>
2017-03-23 11:01:59 +0300

    Added webapi controller for posting "invoice" collections. Automapper package update

---
+++ ApartMgr/Controllers/InvoiceCollectionsApiController.cs
@@ -0,0 +1,43 @@
+﻿using ApartMgr.Data;
+using ApartMgr.Models;
+using ApartMgr.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartMgr.Controllers
+{
+    [Route("api/invoiceCollections")]
+    public class InvoiceCollectionsApiController: Controller
+    {
+        private readonly IInvoiceRepository _invoiceRepository;
+        public InvoiceCollectionsApiController(IInvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateInvoiceCollection([FromBody] IEnumerable<InvoiceCreate> model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+            var entities = Mapper.Map<IEnumerable<Invoice>>(model);
+            foreach (var item in entities)
+            {
+                _invoiceRepository.Create(item);
+            }
+            if (!_invoiceRepository.Commit())
+            {
+                return StatusCode(500, "Failed create new invoice");
+            }
+            return Ok();
+        }
+
+    }
+}
