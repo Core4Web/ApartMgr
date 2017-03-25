6d5172940c1e7aa1d9a16e912106a018d4d46782
Core4Web <core.4web@gmail.com>
2017-03-25 23:51:48 +0300

    Encoding test

---
+++ Database/SampleData1.sql
@@ -0,0 +1,37 @@
+USE ApartMgrDB
+GO
+DELETE FROM [dbo].[Invoices]
+DELETE FROM [dbo].[Periods]
+GO
+SET IDENTITY_INSERT [dbo].[Periods] ON
+GO
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(1, N'Январь')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(2, N'Февраль')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(3, N'Март')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(4, N'Апрель')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(5, N'Май')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(6, N'Июнь')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(7, N'Июль')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(8, N'Август')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(9, N'Сентябрь')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(10, N'Октябрь')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(11, N'Ноябрь')
+INSERT INTO [dbo].[Periods] (Id, PeriodName) VALUES(12, N'Декабрь')
+GO
+SET IDENTITY_INSERT [dbo].[Periods] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Invoices] ON
+GO
+INSERT INTO [dbo].[Invoices] (Id, Number, Account, PeriodId, Year) 
+  VALUES(1, N'1234-F56', N'71', 3, 2016)
+INSERT INTO [dbo].[Invoices] (Id, Number, Account, PeriodId, Year)
+  VALUES(2, N'80-895-12', N'5435454343454354354534', 7, 2016)
+INSERT INTO [dbo].[Invoices] (Id, Number, Account, PeriodId, Year) 
+  VALUES(3, N'123456', N'0000000000', 8, 2016)
+INSERT INTO [dbo].[Invoices] (Id, Number, Account, PeriodId, Year) 
+  VALUES(4, N'145', N'946546', 7, 2016)
+INSERT INTO [dbo].[Invoices] (Id, Number, Account, PeriodId, Year) 
+  VALUES(5, N'АБ567', N'71018946546', 5, 2016)
+GO
+SET IDENTITY_INSERT [dbo].[Invoices] OFF
+GO
